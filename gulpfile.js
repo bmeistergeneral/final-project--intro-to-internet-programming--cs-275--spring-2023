f0d349a9d5affbe9daff331ae099aaa11a9690de
bmeistergeneral <109158688+bmeistergeneral@users.noreply.github.com>
2023-04-28 15:01:01 -0400

    Installed packages using same gulpfile from assignment 3

---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+const { src, dest, series, watch } = require(`gulp`),
+    CSSLinter = require(`gulp-stylelint`),
+    jsLinter = require(`gulp-eslint`),
+    babel = require(`gulp-babel`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload,
+    htmlCompressor = require(`gulp-htmlmin`),
+    jsCompressor = require(`gulp-uglify`),
+    cssCompressor = require(`gulp-clean-css`);
+
+let lintCSS = () => {
+    return src(`./styles/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
+let lintJS = () => {
+    return src(`./scripts/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let transpileJSForDev = () => {
+    return src(`./scripts/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        server: {
+            baseDir: [
+                `./`
+            ]
+        }
+    });
+
+    watch(`./styles/*.css`, series(lintCSS))
+        .on(`change`, reload);
+
+    watch(`./scripts/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`./*.html`)
+        .on(`change`, reload);
+};
+
+let compressHTML = () => {
+    return src([`./*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForProd = () => {
+    return src(`./scripts/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+let compressCSS = () => {
+    return src([`./styles/*.css`])
+        .pipe(cssCompressor({compatibility: `ie8`}))
+        .pipe(dest(`prod/styles`));
+};
+
+exports.lintCSS = lintCSS;
+exports.lintJS = lintJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compressHTML = compressHTML;
+exports.transpileJSForProd = transpileJSForProd;
+exports.compressCSS = compressCSS;
+
+exports.default = series(
+    lintCSS,
+    lintJS,
+    transpileJSForDev,
+    serve
+);
+
+exports.build = series(
+    compressHTML,
+    transpileJSForProd,
+    compressCSS
+);
