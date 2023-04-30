e922d1eed0999fa06f0f04d6762bd1a85a917f01
bmeistergeneral <109158688+bmeistergeneral@users.noreply.github.com>
2023-04-30 16:27:17 -0400

    Validated number input for matrix size.

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,23 @@
 window.onload = () => {
-    // Add your code below
+
+  let matrixSize = 0;
+
+  while (matrixSize == 0) {
+    let input = window.prompt("What size is your square matrix?");
+    let inputAsNumber = Number.parseInt(input, 10);
+
+    // Added validation for ensuring numbers aren't greater than 40 because I don't want website overload.
+    if (!Number.isInteger(inputAsNumber)) {
+      alert("Invalid input. Please try again...");
+    } else if (inputAsNumber < 2) {
+      alert("Integers less than 2 are not allowed. Please try again.");
+    } else if (inputAsNumber > 40) {
+      alert("Integers greater than 40 are not allowed. Please try again.");
+    } else {
+      matrixSize = inputAsNumber;
+    }
+  }
+
+  alert(matrixSize);
 };
+
