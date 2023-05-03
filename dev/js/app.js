6227bbe0cc47e96d1320ed465983e69265e7df6c
bmeistergeneral <109158688+bmeistergeneral@users.noreply.github.com>
2023-05-03 16:48:08 -0400

    Fixed JavaScript lint errors

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,85 +1,86 @@
 window.onload = () => {
 
-  let matrixSize = 0;
+    let matrixSize = 0;
 
-  while (matrixSize == 0) {
-    let input = window.prompt("What size is your square matrix?");
-    let inputAsNumber = Number.parseInt(input, 10);
+    while (matrixSize == 0) {
+        let input = window.prompt(`What size is your square matrix?`);
+        let inputAsNumber = Number.parseInt(input, 10);
 
-    // Added validation for ensuring numbers aren't greater than 40 because I don't want website overload.
-    if (!Number.isInteger(inputAsNumber)) {
-      alert("Invalid input. Please try again...");
-    } else if (inputAsNumber < 2) {
-      alert("Integers less than 2 are not allowed. Please try again.");
-    } else if (inputAsNumber > 40) {
-      alert("Integers greater than 40 are not allowed. Please try again.");
-    } else {
-      matrixSize = inputAsNumber;
+        // Added validation for ensuring numbers aren't greater than 40 because 
+        // I don't want website overload.
+        if (!Number.isInteger(inputAsNumber)) {
+            alert(`Invalid input. Please try again...`);
+        } else if (inputAsNumber < 2) {
+            alert(`Integers less than 2 are not allowed. Please try again.`);
+        } else if (inputAsNumber > 40) {
+            alert(`Integers greater than 40 are not allowed. Please try again.`);
+        } else {
+            matrixSize = inputAsNumber;
+        }
     }
-  }
 
-  let originalMatrix = document.getElementById("original-matrix");
+    let originalMatrix = document.getElementById(`original-matrix`);
 
-  let table = document.createElement("table");
-  originalMatrix.appendChild(table);
+    let table = document.createElement(`table`);
+    originalMatrix.appendChild(table);
 
-  let tableBody = document.createElement("tbody");
-  table.appendChild(tableBody);
+    let tableBody = document.createElement(`tbody`);
+    table.appendChild(tableBody);
 
-  let value = 0;
+    let value = 0;
 
-  for (let i = 0; i < matrixSize; i++) {
+    for (let i = 0; i < matrixSize; i++) {
     
-    let row = document.createElement("tr");
+        let row = document.createElement(`tr`);
     
-    for (let j = 0; j < matrixSize; j++) {
+        for (let j = 0; j < matrixSize; j++) {
 
-      value += 1;
-      let cell = document.createElement("td");
+            value += 1;
+            let cell = document.createElement(`td`);
 
-      if ((i + j) == (matrixSize - 1)) {
-        cell.classList.add("highlighted");
-      }
+            if ((i + j) == (matrixSize - 1)) {
+                cell.classList.add(`highlighted`);
+            }
       
-      let cellText = document.createTextNode(value);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+            let cellText = document.createTextNode(value);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        tableBody.appendChild(row);
     }
 
-    tableBody.appendChild(row);
-  }
+    let reverseMatrix = document.getElementById(`flipped-matrix`);
 
-  let reverseMatrix = document.getElementById("flipped-matrix");
+    let reverseTable = document.createElement(`table`);
+    reverseMatrix.appendChild(reverseTable);
 
-  let reverseTable = document.createElement("table");
-  reverseMatrix.appendChild(reverseTable);
+    let reverseTableBody = document.createElement(`tbody`);
+    reverseTable.appendChild(reverseTableBody);
 
-  let reverseTableBody = document.createElement("tbody");
-  reverseTable.appendChild(reverseTableBody);
+    let reverseValue = matrixSize * matrixSize;
 
-  let reverseValue = matrixSize * matrixSize;
-
-  for (let i = 0; i < matrixSize; i++) {
+    for (let i = 0; i < matrixSize; i++) {
     
-    let row = document.createElement("tr");
+        let row = document.createElement(`tr`);
     
-    for (let j = 0; j < matrixSize; j++) {
+        for (let j = 0; j < matrixSize; j++) {
 
-      let cell = document.createElement("td");
+            let cell = document.createElement(`td`);
 
-      if ((i + j) == (matrixSize - 1)) {
-        cell.classList.add("highlighted");
-      }
+            if ((i + j) == (matrixSize - 1)) {
+                cell.classList.add(`highlighted`);
+            }
       
-      let cellText = document.createTextNode(reverseValue);
-      cell.appendChild(cellText);
-      row.appendChild(cell);
+            let cellText = document.createTextNode(reverseValue);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
 
-      reverseValue -= 1;
+            reverseValue -= 1;
+        }
+
+        reverseTableBody.appendChild(row);
     }
-
-    reverseTableBody.appendChild(row);
-  }
 
 };
 
